eca3c9b4472eb15f0c84e9e43585dd49b3bcb0d8
Elior Matan <elior.matan92@gmail.com>
2020-11-26 16:36:15 +0200

    update LocalApp

--- src/main/java/LocalApp.java
+++ src/main/java/LocalApp.java
@@ -11,7 +11,6 @@ import software.amazon.awssdk.services.sqs.model.*;
 
 import java.nio.file.Paths;
 import java.util.Base64;
-import java.util.Date;
 import java.util.List;
 
 public class LocalApp {
@@ -19,15 +18,19 @@ public class LocalApp {
     private static Ec2Client ec2;
     private static S3Client s3;
     private static SqsClient sqs;
+    private static int filesRatio;
     private static final String amiId = "ami-076515f20540e6e0b";
 
 
     public static void main(String[] args) {
         final String input = args[1];
         final String output = args[2];
+        filesRatio = Integer.parseInt(args[3]);
         final Region region = Region.US_WEST_2;
         final String bucket = "bucket_ocr";
-        final String queue = "queue" + new Date().getTime();
+        final String local2ManagerQ = "local2ManagerQ";
+        final String manager2LocalQ = "manager2LocalQ";
+        final String localId = "local"+System.currentTimeMillis();
         final String new_task = "new task";
         final String done_task = "done task";
         final String arguments =
@@ -48,12 +51,14 @@ public class LocalApp {
 
             //define sqs
             sqs = SqsClient.builder().region(region).build();
-            String queueUrl = createQueueRequestAndGetUrl(queue);
+            String l2m_qUrl = createQueueRequestAndGetUrl(local2ManagerQ);
+            String m2l_qUrl = createQueueRequestAndGetUrl(manager2LocalQ);
 
             //upload the input file to s3
             s3.putObject(PutObjectRequest.builder().bucket(bucket).key(input).acl(ObjectCannedACL.PUBLIC_READ).build(),
                     Paths.get(input));
 
+                //get s3 input url - todo:delete
                 /*s3.getUrl(bucket, input).toExternalForm();
                 GetUrlRequest request = GetUrlRequest.builder().bucket(bucket).key(key).build();
                 String inputS3Path = s3.getUrl(bucket, key).toExternalForm();
@@ -69,7 +74,7 @@ public class LocalApp {
                 URL url = utilities.getUrl(request);*/
 
             //send the location of the file in s3 to the queue (using $ as a delimiter)
-            sendMessage(queueUrl, new_task + "$" + bucket + "$" + input);
+            sendMessage(l2m_qUrl, new_task + "$" + bucket + "$" + input + "$" + localId + "$" + filesRatio);
 
 
             //check if a 'Manager' node is active on the EC2 cloud. If it is not, the application will start the manager node.
@@ -77,13 +82,13 @@ public class LocalApp {
 
             while (!done) {
                 // receive messages from the queue
-                List<Message> messages = receiveMessages(queueUrl);
+                List<Message> messages = receiveMessages(m2l_qUrl);
                 for (Message m : messages) {
                     String[] bodyArr = m.body().split("$");
                     //check for 'done task' message
                     if (bodyArr[0].equals(done_task)) {
                         //delete message
-                        deleteMessage(queueUrl, m);
+                        deleteMessage(m2l_qUrl, m);
                         //get s3 location of the output file
                         outputS3Path = bodyArr[1];
                         done = true;
@@ -96,12 +101,14 @@ public class LocalApp {
                 }
             }
 
-            // download summary file from S3 and write it to output file (assuming it comes in html representation from the manager)
+            //todo: make sure the summary file in s3 (uploaded by the manager)is already in html format
+
+            // download summary file from S3 and write it to output file
             s3.getObject(GetObjectRequest.builder().bucket(bucket).key(outputS3Path).build(),
                     ResponseTransformer.toFile(Paths.get(output)));
 
             if (terminate)
-                sendMessage(queueUrl, "terminate");
+                sendMessage(l2m_qUrl, "terminate");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -202,35 +209,50 @@ public class LocalApp {
     }
     private static boolean managerIsActive() {
         boolean isActive=false;
-        //todo: check if manager is already active =  write function managerIsActive
-          /*
-        You can check if the manager is running by listing all your running instances, and checking if one of them is a manager.
-        Use the "tags" feature of Amazon EC2 API to mark a specific instance as one running a Manager:
-        -using tags
-        -CreateTagsRequest API
-        */
-        //todo
+
+        try {
+
+            //option 1 - filter instances by manager tag
+            DescribeInstancesRequest request = DescribeInstancesRequest.builder().maxResults(1).filters(Filter.builder().name("tag:Manager").build()).build();
+            DescribeInstancesResponse response = ec2.describeInstances(request);
+
+            //option 2 - get all instance's tags , then search for manager
+            /*DescribeTagsResponse response = ec2.describeTags();
+            List<TagDescription> tags = response.tags();
+            if(tags.contains("Manager"))
+                isActive=true;
+             */
+
+            if(!response.reservations().isEmpty())
+                isActive=true;
+            } catch (Ec2Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+
         return isActive;
     }
 
     private static String getUserDataScript() {
         //todo: write USER DATA script string
-        /*
-            String userData = "";
-            userData = userData + "#cloud-boothook\n";
-            userData = userData + "#!/bin/bash\n";
-            userData = userData + "sudo java -jar /home/ubuntu/Manager/Manager-1.0.jar " + bucketName + " " +
-                    queueName + " " + arn + " " + keyName + " " + securityGroup + " " + workerAMI + " " + managerAMI;
-            String base64UserData = null;
-            try {
-                base64UserData = new String(Base64.encodeBase64(userData.getBytes("UTF-8")), "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            return base64UserData;
-        }
-         */
-        return "";
+
+            String userData = "#!/bin/bash\n";
+            //???
+            userData = userData + "java -jar Manager.jar";
+
+      /*  String userData = "#!/bin/bash\n" +
+                "aws s3 cp s3://" + JAR_BUCKET_NAME + "/manager.jar manager.jar\n" +
+                "java -jar manager.jar"; */
+
+        /*public static final String USER_DATA_MANAGER = "#!/bin/bash\n" +
+                "sudo mkdir /home/ass/\n" +
+                "sudo aws s3 cp s3://the-gabay-jar/Manager.jar /home/ass/\n" +
+                "sudo /usr/bin/java -jar /home/ass/Manager.jar\n" +
+                "shutdown -h now";*/
+
+
+//https://aws.amazon.com/getting-started/hands-on/backup-to-s3-cli/
+
+        return userData;
     }
 
 
