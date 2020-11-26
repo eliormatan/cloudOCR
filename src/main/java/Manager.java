c03a2414c8e225488a6d36906c493f18c20d2fb3
ibrahim <awadibra86@gmail.com>
2020-11-26 16:28:00 +0200

    updated
     Conflicts:
    	src/main/java/ReadOCR.java

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -14,7 +14,8 @@ import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequestEntry;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
@@ -26,6 +27,9 @@ public class Manager {
     private static final String manager2LocalQ = "manager2LocalQ";
     private static final String manager2WorkerQ = "manager2WorkerQ";
     private static final String worker2ManagerQ = "worker2ManagerQ";
+    private static final String new_image_task = "new_image_task";
+    private static final String done_ocr_task = "done_ocr_task";
+
 
     public static void main (String args[]) {
         //todo: maybe add threads?
@@ -34,23 +38,17 @@ public class Manager {
         sqs = SqsClient.builder().region(region).build();
         s3 = S3Client.builder().region(region).build();
 
-        //todo: to get the queue that sends new tasks from local app to manager we need to use the same name
-        //queue access by name, i called it local2ManagerQ
+        //todo: to get the queue that sends new tasks from local app to manager we need to use the same name ,
+        // queue access by name, i called it local2ManagerQ
 
         //local 2 manager queue url = local2ManagerQUrl
-        GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
-                .queueName(local2ManagerQ)
-                .build();
-        String local2ManagerQUrl = sqs.getQueueUrl(getQueueRequest).queueUrl();
-
+        String local2ManagerQUrl = getQueueRequestAndGetUrl(local2ManagerQ);
         //manager 2 local queue url = manager2LocalQUrl
-        getQueueRequest = GetQueueUrlRequest.builder()
-                .queueName(manager2LocalQ)
-                .build();
-        String manager2LocalQUrl = sqs.getQueueUrl((getQueueRequest)).queueUrl();
+        String manager2LocalQUrl = getQueueRequestAndGetUrl(manager2LocalQ);
+
 
         //create queues to send & receive messages from workers
-        //todo: createQueueRequestAndGetUrl function is taken from LocalApp, is it shared or do we have to copy code?
+        //createQueueRequestAndGetUrl function is taken from LocalApp
         String manager2WorkerQUrl = createQueueRequestAndGetUrl(manager2WorkerQ);
         String worker2ManagerQUrl = createQueueRequestAndGetUrl(worker2ManagerQ);
 
@@ -60,19 +58,20 @@ public class Manager {
         String key;
         String localId;
         while(!terminate){
+            //STEP 4: Manager downloads list of images
             //receive messages from localApp
             ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
-                    .queueUrl(manager2LocalQUrl)
+                    .queueUrl(local2ManagerQUrl)
                     .build();
             List<Message> messages = sqs.receiveMessage(receiveRequest).messages();
             for (Message message : messages) {
                 String messageBody = message.body();
-                //todo: define how the new task message is sent from local app to the manager
-                //should receive bucket, key, and local app id to access new task file in s3
-                //in case of multiple local apps, each local app will have its own queues
-                //based on its own ID
+                //todo: define how the new task message is sent from local app to the manager,
+                // should receive bucket, key, and local app id to access new task file in s3,
+                // in case of multiple local apps, each local app will have its own queues,
+                // based on its own ID
 
-                //todo: terminate or not?
+                //todo: terminate or not? how to receive terminate message
 
                 //todo: split new task messages and extract needed info such as bucket, key, localID
 
@@ -103,11 +102,69 @@ public class Manager {
                 DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(bucket).key(key).build();
                 s3.deleteObject(deleteObjectRequest);
 
-                //todo: the rest
+                //STEP 5: Manager creates an SQS message for each URL in the list of images
+                //read lines (image links) from input file and create new image tasks for workers out of each line
+                int numberOfLines = 0;
+                try{
+                    BufferedReader reader = new BufferedReader(new FileReader("input"+localId+".txt"));
+                    String line = reader.readLine();
+                    while (line != null){
+                        numberOfLines++;
+                        queueNewImageTask(manager2WorkerQUrl, line, localId);
+                        //onto the next line
+                        line = reader.readLine();
+                    }
+                    reader.close();
+                    //done reading all new image task lines, delete the file since we are done with it
+                    File input = new File("input"+localId+".txt");
+                    input.delete();;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //delete the "new task" message from local2managerQUrl sqs queue
+                DeleteMessageRequest deleteRequest = DeleteMessageRequest.builder()
+                        .queueUrl(local2ManagerQUrl)
+                        .receiptHandle(message.receiptHandle())
+                        .build();
+                sqs.deleteMessage(deleteRequest);
+
+                //STEP 6: Manager bootstraps nodes to process messages
+                //todo: start workers and bootstrap them
+
             }
         }
     }
-    //todo: add a function to send new image task to workers
+    //send new image task to workers
+    private static void queueNewImageTask(String queue, String line, String localId){
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                .queueUrl(queue)
+                .messageBody(new_image_task + "$" + localId + "$" + line)
+                .delaySeconds(5)
+                .build();
+        sqs.sendMessage(sendMessageRequest);
+    }
+
     //todo: add a function that receives done OCR task from workers
     //todo: add a function to merge outputs from workers
+    private static String createQueueRequestAndGetUrl(String queue) {
+        try {
+            CreateQueueRequest request = CreateQueueRequest.builder()
+                    .queueName(queue)
+                    .build();
+            sqs.createQueue(request);
+            GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
+                    .queueName(queue)
+                    .build();
+            return sqs.getQueueUrl(getQueueRequest).queueUrl();
+        } catch (QueueNameExistsException e) {
+            throw e;
+        }
+    }
+    private static String getQueueRequestAndGetUrl(String queue) {
+        GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
+                .queueName(queue)
+                .build();
+        String queueUrl = sqs.getQueueUrl(getQueueRequest).queueUrl();
+        return queueUrl;
+    }
 }
